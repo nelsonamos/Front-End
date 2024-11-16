7bec7b6329e9f7bc6c23dc3afb524d72072050c1
nelson amos <nelsonamos52@gmail.com>
2024-11-16 07:08:56 +0400

    deploy

---
+++ src/main.js
@@ -0,0 +1,9 @@
+// main.js
+new Vue({
+    el: '#app',
+    data: {
+      message: 'Hello Vue!',
+    },
+    // your other Vue instance properties here...
+  });
+  
---
+++ vue.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    publicPath: '/<Front-End>/',
+  };
+  
