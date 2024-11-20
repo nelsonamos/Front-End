b34cc24e63de2e1d4640428aa4f14d75af0d3b2f
nelson amos <nelsonamos52@gmail.com>
2024-11-20 06:34:28 +0400

    new stage

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,3 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? '/<Front-End>/' : '/'
-  }
-  
+  publicPath: process.env.NODE_ENV === 'production' ? '/Front-End/' : '/'
+};
